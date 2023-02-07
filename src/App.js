2613b722121c00728d88e32777649ac5b8667ac1
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-02-07 23:32:33 +0000

    Fetch post in App.js

--- src/App.js
+++ src/App.js
@@ -4,31 +4,27 @@ import { useState, useEffect } from "react";
 import Block from "./components/Block";
 
 function App() {
-  const URL =
-    "http://localhost:5000/posts/63dbaf9412e514c68d95c4ba/blocks/63df747cbffb7cf2db872b85";
+	const URL = "http://localhost:5000/posts/63dbaf9412e514c68d95c4ba";
 
-  const [block, setBlock] = useState({
-    type: "subtitle",
-    text: "...Loading",
-  });
+	const [post, setPost] = useState({
+		title: "...Loading",
+		content: [],
+	});
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(URL, { mode: "cors" });
-      const json = await response.json();
-      return json;
-    }
-    const res = fetchData();
-    res.then((res) => {
-      setBlock(res.block);
-    });
-  }, []);
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch(URL, { mode: "cors" });
+			const json = await response.json();
+			setPost(json.post);
+		}
+		fetchData();
+	}, []);
 
-  return (
-    <div className="App">
-      <Block block={block} />
-    </div>
-  );
+	return (
+		<div className="App">
+			{post.content.length > 0 && <Block block={post.content[0]} />}
+		</div>
+	);
 }
 
 export default App;

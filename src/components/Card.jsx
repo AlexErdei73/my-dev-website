96409431767bf06b32721f793d69aee4749f71d0
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-02 13:31:02 +0000

    Make PostCard.less and move styles from Card.less

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,30 +2,14 @@ import React from "react";
 import "./Card.css";
 
 const Card = (props) => {
-  const {
-    headerText,
-    mainText,
-    footerTextLeft,
-    footerTextRight,
-    onClick,
-  } = props;
+  const { headerText, footerTextLeft, footerTextRight, children } = props;
 
   return (
     <div className="card">
       <div className="card__header">
         <div>{headerText}</div>
       </div>
-      <div className="card__body">
-        <div className="card__title">
-          <h2 className="card__title">{mainText}</h2>
-        </div>
-        <div className="card__button-container">
-          <button className="card__button" onClick={onClick}>
-            View
-          </button>
-          <button className="card__button--delete-lg">Delete</button>
-        </div>
-      </div>
+      {children}
       <div className="card__footer">
         <div>{footerTextLeft}</div>
         <div>{footerTextRight}</div>
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "./Card";
 import "./PostCard.css";
 
 const PostCard = (props) => {
@@ -7,28 +8,25 @@ const PostCard = (props) => {
   const { title, author, createdAt, updatedAt } = post;
 
   return (
-    <div className="post-card">
-      <div className="post-card__header">
-        <div className="post-card__header-text">
-          <div>By {author.username}</div>
-        </div>
-      </div>
+    <Card
+      headerText={`By ${author.username}`}
+      footerTextLeft={`Created:${createdAt && createdAt.slice(0, 10)}`}
+      footerTextRight={`Updated:${updatedAt && updatedAt.slice(0, 10)}`}
+    >
       <div className="post-card__body">
         <div className="post-card__title">
-          <h2 className="post-card__title">{title}</h2>
+          <h2>{title}</h2>
         </div>
         <div className="post-card__button-container">
           <button className="post-card__button" onClick={() => onClick(post)}>
             View
           </button>
-          <button className="post-card__button--delete-lg">Delete</button>
+          {edit && (
+            <button className="post-card__button--delete-lg">Delete</button>
+          )}
         </div>
       </div>
-      <div className="post-card__footer">
-        <div>Created:{createdAt && createdAt.slice(0, 10)}</div>
-        <div>Updated:{updatedAt && updatedAt.slice(0, 10)}</div>
-      </div>
-    </div>
+    </Card>
   );
 };
 

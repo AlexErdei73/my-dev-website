9481c7f1c6507c70ce5395e30e44fed310deccd9
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-02 23:59:16 +0000

    Add variant prop to Card and style for danger

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import "./Card.css";
 
 const Card = (props) => {
-  const { headerText, footerTextLeft, footerTextRight, children } = props;
+	const {
+		variant,
+		headerText,
+		footerTextLeft,
+		footerTextRight,
+		children,
+	} = props;
 
-  return (
-    <div className="card">
-      <div className="card__header">
-        <div>{headerText}</div>
-      </div>
-      {children}
-      <div className="card__footer">
-        <div>{footerTextLeft}</div>
-        <div>{footerTextRight}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="card">
+			<div className={`card__header ${variant && variant}`}>
+				<div>{headerText}</div>
+			</div>
+			{children}
+			<div className={`card__footer ${variant && `footer-${variant}`}`}>
+				<div>{footerTextLeft}</div>
+				<div>{footerTextRight}</div>
+			</div>
+		</div>
+	);
 };
 
 export default Card;

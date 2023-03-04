b6e9f6a76ef78a15872b155d972c0f164f22d1c2
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-04 21:03:50 +0000

    Only show published posts in PostView publicly

--- src/components/PostsView.jsx
+++ src/components/PostsView.jsx
@@ -4,41 +4,44 @@ import "./PostsView.css";
 import { useNavigate } from "react-router-dom";
 
 const PostsView = (props) => {
-	const { posts, setIndex, edit, open } = props;
-	const navigate = useNavigate();
+  const { posts, setIndex, edit, open } = props;
+  const navigate = useNavigate();
 
-	const [postIndex, setPostIndex] = useState(-1);
+  const [postIndex, setPostIndex] = useState(-1);
 
-	function getIndex(searchedPost) {
-		return posts.findIndex((post) => post._id === searchedPost._id);
-	}
+  function getIndex(searchedPost) {
+    return posts.findIndex((post) => post._id === searchedPost._id);
+  }
 
-	function handleView(viewedPost) {
-		setPostIndex(getIndex(viewedPost));
-	}
+  function handleView(viewedPost) {
+    setPostIndex(getIndex(viewedPost));
+  }
 
-	useEffect(() => {
-		if (postIndex === -1) return;
-		setIndex(postIndex);
-		navigate("/post");
-	}, [postIndex]);
+  useEffect(() => {
+    if (postIndex === -1) return;
+    setIndex(postIndex);
+    navigate("/post");
+  }, [postIndex]);
 
-	return (
-		<div className="posts-view">
-			{posts.map((post) => (
-				<PostCard
-					key={post._id}
-					post={post}
-					onClick={handleView}
-					edit={edit}
-					open={() => {
-						setIndex(getIndex(post));
-						open();
-					}}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className="posts-view">
+      {posts.map(
+        (post) =>
+          (post.isPublished || edit) && (
+            <PostCard
+              key={post._id}
+              post={post}
+              onClick={handleView}
+              edit={edit}
+              open={() => {
+                setIndex(getIndex(post));
+                open();
+              }}
+            />
+          )
+      )}
+    </div>
+  );
 };
 
 export default PostsView;

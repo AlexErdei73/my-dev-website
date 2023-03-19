a7b13b9774d8c790d57b383dfd1610e116431b31
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-19 11:05:14 +0000

    Updates

---
+++ precache-manifest.48035d59d601ceccea1dbead32b8439c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c19cd8b7978f4d4af233",
+    "url": "/my-dev-website/static/css/main.f2a05169.chunk.css"
+  },
+  {
+    "revision": "c19cd8b7978f4d4af233",
+    "url": "/my-dev-website/static/js/main.c19cd8b7.chunk.js"
+  },
+  {
+    "revision": "cbadaf8f64818079c3db",
+    "url": "/my-dev-website/static/js/1.cbadaf8f.chunk.js"
+  },
+  {
+    "revision": "35af01312e2825208654",
+    "url": "/my-dev-website/static/js/2.35af0131.chunk.js"
+  },
+  {
+    "revision": "41cc809652b82759f0a5",
+    "url": "/my-dev-website/static/js/runtime~main.41cc8096.js"
+  },
+  {
+    "revision": "bd87dc5b7355bf4f5fa78f3255d3c64e",
+    "url": "/my-dev-website/index.html"
+  }
+];

6b9e56ffda33b7ae007c1d6ee4d10b4619f8d9e7
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-19 11:59:33 +0000

    Correct paths

--- src/components/AppMenu.jsx
+++ src/components/AppMenu.jsx
@@ -3,34 +3,36 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 const AppMenu = (props) => {
-  const LOGO = "Alex73 DEV";
+	const LOGO = "Alex73 DEV";
 
-  const { loginSuccess } = props;
+	const { loginSuccess } = props;
 
-  return (
-    <nav className="nav">
-      <ul>
-        <li className="nav__logo">
-          <Link to="/">{LOGO}</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="/about">About</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="/post">Post</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="/login">Login</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="/signup">{loginSuccess ? "Update User" : "Sign Up"}</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className="nav">
+			<ul>
+				<li className="nav__logo">
+					<Link to="/my-dev-website/">{LOGO}</Link>
+				</li>
+				<li className="nav__item">
+					<Link to="/my-dev-website/about">About</Link>
+				</li>
+				<li className="nav__item">
+					<Link to="/my-dev-website/">Home</Link>
+				</li>
+				<li className="nav__item">
+					<Link to="/my-dev-website/post">Post</Link>
+				</li>
+				<li className="nav__item">
+					<Link to="/my-dev-website/login">Login</Link>
+				</li>
+				<li className="nav__item">
+					<Link to="/my-dev-website/signup">
+						{loginSuccess ? "Update User" : "Sign Up"}
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default AppMenu;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,99 +4,98 @@ import ErrorMsg from "./ErrorMsg";
 import "./Signup.css";
 
 const Signup = (props) => {
-  const { loginState, submit, update, response, deleteErrors } = props;
+	const { loginState, submit, update, response, deleteErrors } = props;
 
-  const { user: currentUser, success: loginSuccess } = loginState;
-  const { success, errors } = response;
+	const { user: currentUser, success: loginSuccess } = loginState;
+	const { success, errors } = response;
 
-  const [user, setUser] = useState(currentUser);
+	const [user, setUser] = useState(currentUser);
 
-  useEffect(() => {
-    return () => deleteErrors();
-  }, []);
+	useEffect(() => {
+		return () => deleteErrors();
+	}, []);
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!success) return;
-    navigate("/login");
-  }, [success]);
+	useEffect(() => {
+		if (!success) return;
+		navigate("/my-dev-website/login");
+	}, [success]);
 
-  function handleChange(event) {
-    const fieldInput = event.target;
-    const newUser = {
-      ...user,
-      [fieldInput.id]: fieldInput.value,
-    };
-    setUser(newUser);
-  }
+	function handleChange(event) {
+		const fieldInput = event.target;
+		const newUser = {
+			...user,
+			[fieldInput.id]: fieldInput.value,
+		};
+		setUser(newUser);
+	}
 
-  return (
-    <form
-      className="signup"
-      onSubmit={(event) => {
-        event.preventDefault();
-        loginSuccess ? update(user) : submit(user);
-      }}
-      validate="true"
-    >
-      <h1 className="signup__title">
-        {loginSuccess ? "Update User" : "Sign Up"}
-      </h1>
-      <label htmlFor="username">Username*</label>
-      <input
-        type="text"
-        id="username"
-        className="signup__username"
-        onChange={handleChange}
-        value={user.username}
-        required
-      />
-      {!loginSuccess && <label htmlFor="password">Password*</label>}
-      {!loginSuccess && (
-        <input
-          type="text"
-          id="password"
-          className="signup__password"
-          onChange={handleChange}
-          value={user.password}
-          required
-        />
-      )}
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        id="name"
-        className="signup__name"
-        onChange={handleChange}
-        value={user.name}
-      />
-      <label htmlFor="jobTitle">Job Title</label>
-      <input
-        type="text"
-        id="jobTitle"
-        className="signup__job-title"
-        onChange={handleChange}
-        value={user.jobTitle}
-      />
-      <label htmlFor="bio">Bio</label>
-      <textarea
-        name="bio"
-        id="bio"
-        cols="30"
-        rows="10"
-        className="submit_bio"
-        onChange={handleChange}
-        value={user.bio}
-      />
-      <button type="submit" className="signup__button">
-        Submit
-      </button>
-      {errors &&
-        errors.length !== 0 &&
-        errors.map((error, index) => <ErrorMsg msg={error.msg} key={index} />)}
-    </form>
-  );
+	return (
+		<form
+			className="signup"
+			onSubmit={(event) => {
+				event.preventDefault();
+				loginSuccess ? update(user) : submit(user);
+			}}
+			validate="true">
+			<h1 className="signup__title">
+				{loginSuccess ? "Update User" : "Sign Up"}
+			</h1>
+			<label htmlFor="username">Username*</label>
+			<input
+				type="text"
+				id="username"
+				className="signup__username"
+				onChange={handleChange}
+				value={user.username}
+				required
+			/>
+			{!loginSuccess && <label htmlFor="password">Password*</label>}
+			{!loginSuccess && (
+				<input
+					type="text"
+					id="password"
+					className="signup__password"
+					onChange={handleChange}
+					value={user.password}
+					required
+				/>
+			)}
+			<label htmlFor="name">Name</label>
+			<input
+				type="text"
+				id="name"
+				className="signup__name"
+				onChange={handleChange}
+				value={user.name}
+			/>
+			<label htmlFor="jobTitle">Job Title</label>
+			<input
+				type="text"
+				id="jobTitle"
+				className="signup__job-title"
+				onChange={handleChange}
+				value={user.jobTitle}
+			/>
+			<label htmlFor="bio">Bio</label>
+			<textarea
+				name="bio"
+				id="bio"
+				cols="30"
+				rows="10"
+				className="submit_bio"
+				onChange={handleChange}
+				value={user.bio}
+			/>
+			<button type="submit" className="signup__button">
+				Submit
+			</button>
+			{errors &&
+				errors.length !== 0 &&
+				errors.map((error, index) => <ErrorMsg msg={error.msg} key={index} />)}
+		</form>
+	);
 };
 
 export default Signup;

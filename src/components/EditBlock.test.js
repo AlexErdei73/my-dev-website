8d1e7f1d17b9b55a61b47ab4f4a2570c1da19aa3
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-06-18 22:42:26 +0100

    Test click cancel

--- src/components/EditBlock.test.js
+++ src/components/EditBlock.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { screen, render, getByRole } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import EditBlock from "./EditBlock";
 
 test("render paragraph with no link", () => {
@@ -45,4 +46,22 @@ test("render paragraph with links", () => {
 	expect(screen.getByRole("textbox")).toHaveTextContent(
 		"[testlink](https://testlink.com)This is a [secondtestlink](https://testlink-2.com)test paragraph."
 	);
+});
+
+test("click cancel calls cancel", () => {
+	const block = {
+		_id: "test-block-1",
+		text: "This is a test paragraph.",
+		type: "paragraph",
+		links: [],
+		errors: [],
+	};
+	const submit = jest.fn();
+	const cancel = jest.fn();
+
+	render(<EditBlock block={block} submit={submit} cancel={cancel} />);
+	userEvent.click(screen.getByRole("button", { name: "Cancel" }));
+
+	expect(cancel).toBeCalled();
+	expect(screen.getByRole("textbox")).toHaveTextContent(block.text);
 });

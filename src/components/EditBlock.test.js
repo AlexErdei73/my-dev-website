63c3fe3d78dc55c6f321f161fe2436e7e5008ba3
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-06-18 22:19:34 +0100

    Test render paragraph with links

--- src/components/EditBlock.test.js
+++ src/components/EditBlock.test.js
@@ -17,3 +17,32 @@ test("render paragraph with no link", () => {
 
 	expect(screen.getByRole("textbox")).toHaveTextContent(block.text);
 });
+
+test("render paragraph with links", () => {
+	const block = {
+		_id: "test-block-1",
+		text: "This is a test paragraph.",
+		type: "paragraph",
+		links: [
+			{
+				url: "https://testlink.com",
+				description: "testlink",
+				position: 0,
+			},
+			{
+				url: "https://testlink-2.com",
+				description: "secondtestlink",
+				position: 10,
+			},
+		],
+		errors: [],
+	};
+	const submit = jest.fn();
+	const cancel = jest.fn();
+
+	render(<EditBlock block={block} submit={submit} cancel={cancel} />);
+
+	expect(screen.getByRole("textbox")).toHaveTextContent(
+		"[testlink](https://testlink.com)This is a [secondtestlink](https://testlink-2.com)test paragraph."
+	);
+});

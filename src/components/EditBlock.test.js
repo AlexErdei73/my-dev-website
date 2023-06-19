1668e1d29164950ba2bb23dfe4e9a7bbfcd2e8b4
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-06-19 10:13:59 +0100

    Test click submit submits block

--- src/components/EditBlock.test.js
+++ src/components/EditBlock.test.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { screen, render, getByRole } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import EditBlock from "./EditBlock";
+import { act } from "react-dom/test-utils";
 
 test("render paragraph with no link", () => {
 	const block = {
@@ -60,8 +61,41 @@ test("click cancel calls cancel", () => {
 	const cancel = jest.fn();
 
 	render(<EditBlock block={block} submit={submit} cancel={cancel} />);
-	userEvent.click(screen.getByRole("button", { name: "Cancel" }));
+	const cancelBtn = screen.getByRole("button", { name: "Cancel" });
+	act(() => {
+		userEvent.click(cancelBtn);
+	});
 
-	expect(cancel).toBeCalled();
+	expect(cancel).toBeCalledWith();
 	expect(screen.getByRole("textbox")).toHaveTextContent(block.text);
+});
+
+test("click submit submits block", () => {
+	const block = {
+		_id: "test-block-1",
+		text: "This is a test paragraph.",
+		type: "paragraph",
+		links: [],
+		errors: [],
+	};
+	const link = {
+		url: "https://testlink.com",
+		description: "testlink",
+		position: 25,
+	};
+	const submit = jest.fn();
+	const cancel = jest.fn();
+
+	render(<EditBlock block={block} submit={submit} cancel={cancel} />);
+	const linkText = `[${link.description}](${link.url})`;
+	act(() => {
+		// [ and { special characters must be given double to work
+		// see https://testing-library.com/docs/user-event/keyboard/
+		userEvent.type(screen.getByRole("textbox"), "[" + linkText);
+		userEvent.click(screen.getByRole("button", { name: "Submit" }));
+	});
+
+	block.links.push(link);
+	expect(submit).toBeCalledWith(block);
+	expect(screen.getByRole("textbox")).toHaveTextContent(block.text + linkText);
 });
